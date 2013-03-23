81e165849d911875b0dfa1db55c16a17e18dc5be
Marc Villacorta <marc.villacorta@gmail.com>
2013-03-23 21:37:55 +0100

    Adding 'pap' and 'chap' file fragments headers.

--- manifests/config.pp
+++ manifests/config.pp
@@ -16,5 +16,28 @@ class pppd::config {
     $configs   = getvar("${module_name}::params::configs")
     $ifaces    = getvar("${module_name}::ifaces")
 
+    # Define the target files:
+    concat { $configs:
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0600',
+    }
+
+    # Config file headers:
+    concat::fragment {
+        'chap_header':
+            ensure  => present,
+            target  => $configs[0],
+            content => template("${templates}/chap-secrets_header.erb"),
+            order   => '00';
+        'pap_header':
+            ensure  => present,
+            target  => $configs[1],
+            content => template("${templates}/pap-secrets_header.erb"),
+            order   => '00';
+    }
+
+    # Interfaces:
     create_resources(pppd::interface, $ifaces)
 }
